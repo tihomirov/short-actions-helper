5b475d2d99eb3a540962a4dbf4adb64c6c9c685f
Anton <antontikhomirov93@gmail.com>
2023-02-19 09:53:13 +0200

    Use webextension-polyfill for storage

--- src/pages/popup/services/browser-storage.ts
+++ src/pages/popup/services/browser-storage.ts
@@ -1,4 +1,5 @@
 import hexoid from 'hexoid';
+import browser from 'webextension-polyfill';
 import { Commands, Command, PendingCommandForm } from '../types';
 import { ElementData } from '../../../common';
 
@@ -24,55 +25,55 @@ const getCommandId = hexoid();
 
 class BrowserStorageService implements IStorageService {
   async getCommands(): Promise<Commands> {
-    const { commands = [] } = await chrome.storage.sync.get(StorageKey.Commands);
+    const { commands = [] } = await browser.storage.sync.get(StorageKey.Commands);
     return commands.filter((command: Command) => !command.hostname);
   }
   async getCommandsByHostname(hostname?: string): Promise<Commands> {
-    const { commands = [] } = await chrome.storage.sync.get(StorageKey.Commands);
+    const { commands = [] } = await browser.storage.sync.get(StorageKey.Commands);
     return commands.filter((command: Command) => command.hostname === hostname);
   }
 
   async createCommand(command: Omit<Command, 'id'>): Promise<Command> {
-    const { commands = [] } = await chrome.storage.sync.get(StorageKey.Commands);
+    const { commands = [] } = await browser.storage.sync.get(StorageKey.Commands);
     const newCommand = {
       id: getCommandId(),
       ...command,
     };
 
     commands.push(newCommand);
-    await chrome.storage.sync.set({ [StorageKey.Commands]: commands });
+    await browser.storage.sync.set({ [StorageKey.Commands]: commands });
 
     return newCommand;
   }
 
   async deleteCommand(id: string): Promise<void> {
-    const { commands = [] } = await chrome.storage.sync.get(StorageKey.Commands);
+    const { commands = [] } = await browser.storage.sync.get(StorageKey.Commands);
     const filteredCommands = commands.filter((command: Command) => command.id !== id);
 
-    await chrome.storage.sync.set({ [StorageKey.Commands]: filteredCommands });
+    await browser.storage.sync.set({ [StorageKey.Commands]: filteredCommands });
   }
 
   async getPendingCommand(): Promise<PendingCommandForm | undefined> {
-    const { pendingCommand } = await chrome.storage.sync.get(StorageKey.PendingCommand);
+    const { pendingCommand } = await browser.storage.sync.get(StorageKey.PendingCommand);
     return pendingCommand;
   }
 
   async savePendingCommand(command: PendingCommandForm): Promise<PendingCommandForm> {
-    await chrome.storage.sync.set({ [StorageKey.PendingCommand]: command });
+    await browser.storage.sync.set({ [StorageKey.PendingCommand]: command });
     return command;
   }
 
   async removePendingCommand(): Promise<void> {
-    await chrome.storage.sync.remove(StorageKey.PendingCommand);
+    await browser.storage.sync.remove(StorageKey.PendingCommand);
   }
 
   async getInterceptedElement(): Promise<ElementData | undefined> {
-    const { interceptedElement } = await chrome.storage.sync.get(StorageKey.InterceptedElement);
+    const { interceptedElement } = await browser.storage.sync.get(StorageKey.InterceptedElement);
     return interceptedElement;
   }
 
   async removeInterceptedElement(): Promise<void> {
-    await chrome.storage.sync.remove(StorageKey.InterceptedElement);
+    await browser.storage.sync.remove(StorageKey.InterceptedElement);
   }
 }
 
