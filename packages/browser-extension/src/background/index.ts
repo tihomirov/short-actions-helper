ef78dcfd078eee989e21476e99218653efbcc7d8
Anton <antontikhomirov93@gmail.com>
2023-04-11 21:08:40 +0300

    Fix background script

--- packages/browser-extension/src/background/index.ts
+++ packages/browser-extension/src/background/index.ts
@@ -1,1 +1,11 @@
+import browser from 'webextension-polyfill';
+
 console.log('background script');
+
+browser.tabs.onActivated.addListener(function (activeInfo) {
+  console.log('!!! onActivated', activeInfo);
+});
+
+browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  console.log('!!! onUpdated', tabId, changeInfo, tab);
+});
